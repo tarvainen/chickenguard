6fbbb3de76081ef2b9a70ab53def731d21f2d1a3
tarvainen <atte.tarvainen@protacon.com>
2019-03-11 19:14:38 +0200

    Add health url

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ const swaggerUi = require('swagger-ui-express')
 const YAML = require('yamljs')
 const swaggerDocument = YAML.load('./swagger.yaml')
 
+router.use('/healtz', require('./healtz'))
 router.use('/climate-info', require('./climate-info'))
 router.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
